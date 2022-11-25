396478d5c51c6d464e0a302efd996729b2d45505
adrian <aygomez138@gmail.com>
2022-11-24 21:50:42 -0800

    cleaned up a little

--- src/strSearch.s
+++ src/strSearch.s
@@ -6,8 +6,7 @@ noMatches: .asciz "\nNo matches found"
 .global strSearch
 // takes substring in x0
 .text
-// substring is moved to x1, x2 is used as temp, x3 is index counter,
-// 
+// substring is moved to x10, x2 is used as temp, x11 is index counter,
 // x12 is a boolean for whether any substrings were found
 strSearch:
     str lr, [sp, #-16]! // str lr
@@ -17,10 +16,9 @@ strSearch:
     ldr x2, =head
     ldr x2, [x2]
 
-// x11 = index counter, x12 = boolean
+// clearing these registers & moving substr to x10
     mov x11, #0
     mov x12, #0
-// saving copy of substr into x10
     mov x10, x0
     b strSearch__loop
 
@@ -38,7 +36,6 @@ strSearch__loop:
     bl indexOf_3
     cmp x0, -1 
     b.eq strSearch__notMatch
-
     b strSearch__match
      
 
@@ -49,20 +46,18 @@ strSearch__match:
 
 // print index of node containing matched string
     mov x0, x11
-
     ldr x1, =szBuffer 
     bl int64asc
     ldr x0, =szBuffer
     bl putstring
 
+// load node into x2
     ldr x2, [sp], #16
-
-// temp->next
-    add x2, x2, #8 
 // temp=temp->next
+    add x2, x2, #8 
     ldr x2, [x2]
 
-// x12 used as boolean, x3 used as node index
+// x12 used as boolean, x3 used as index
     add x12, x12, #1
     add x11, x11, #1
     b strSearch__loop
@@ -71,18 +66,16 @@ strSearch__match:
 strSearch__notMatch:
     ldr x2, [sp], #16
 
-// temp->next
+// temp =temp->next
     add x2, x2, #8 
-// temp=temp->next
     ldr x2, [x2]
 
 // increment node index
     add x11, x11, #1
     b strSearch__loop
 
-
 strSearch__done:
-  
+// if boolean == 0, no matches were made
   cmp x12, #0
   b.eq strSearch_noMatches
   b strSearch__return
@@ -91,16 +84,10 @@ strSearch_noMatches:
     ldr x0, =noMatches
     bl putstring
     b strSearch__return
-    
-    
 
 strSearch__return:
-
     mov x0, x10
     bl free
     ldr lr, [sp], #16
     ret
-
-    
-
-    
+.end
