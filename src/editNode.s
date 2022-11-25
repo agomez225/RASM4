739d556ad6e4cad4c1bece674c4199d4f709bfdd
adrian <aygomez138@gmail.com>
2022-11-24 16:37:35 -0800

    fixed formatting & commented

--- src/editNode.s
+++ src/editNode.s
@@ -22,45 +22,37 @@ editNode__findNode:
     cmp x0, x1
     b.eq editNode__found
 
-    add x2, x2, #8 // node->next
-    ldr x2, [x2] // load next
-    add x1, x1, #1 // increment counter
+// temp =node->next && counter++
+    add x2, x2, #8
+    ldr x2, [x2] 
+    add x1, x1, #1 
     b editNode__findNode
 
 editNode__found:
 
-    str x2, [sp, #-16]! // store node
-    mov x0, x2 // copy node into x0
-    ldr x0, [x0] // loads str held in node
+// store node
+    str x2, [sp, #-16]! 
+
+// copy node into x0
+    mov x0, x2 
+
+// loads str held in node
+    ldr x0, [x0] 
     bl free 
+
 // prompt user to enter new string
     ldr x0, =enterNewString
     bl putstring
-
     bl getInput
     ldr x0, =szBuffer
-    bl copy // copies szbuffer into malloc string
-    ldr x1, [sp], #16
-    str x0, [x1] // this line may cause an issue
 
+// copies szbuffer into malloc string
+    bl copy 
+    ldr x1, [sp], #16
+
+// node->data = new string
+    str x0, [x1] 
     ldr lr, [sp], #16
     ret
-
-
-
-/*to do: 
-1a. str all registers b4 calling malloc
-1. free memory of data segment of node
-
-2. get user input > szBuffer
-3. sizeOf(szBuffer)
-4. malloc
-5. copy szBuffer -> new address
-6. str new address -> node */
-
-
-
-    ret
-
 
 
